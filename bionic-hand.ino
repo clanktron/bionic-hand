5a59243878550116f7034fca531821d64af8d9a5
clayton <crdcastro5@gmail.com>
2022-11-20 15:40:01 -0800

    migrate to single openhand func
    fix
    
    .

--- bionic-hand.ino
+++ bionic-hand.ino
@@ -1,15 +1,13 @@
 #include <Servo.h>
 
-#define OPENBUTTON_PARTIAL 1
-#define OPENBUTTON_FULL 2
 #define CLOSEBUTTON_PARTIAL 3
 #define CLOSEBUTTON_FULL 4
+#define OPENBUTTON 2
 
 #define SERVO_INDEX 5
 #define SERVO_REM 6
 
 #define FSR_PIN A0
-//#define FSR_THUMB
 
 // Force reading at which index/thumb should stop squeezing grape
 int fsr_threshold = ;
@@ -20,10 +18,9 @@ int index_full_threshold = ;
 int rem_full_threshold = ;
 
 // Initialization of control button states
-int openbuttonstate_partial = 0;
 int closebuttonstate_partial = 0;
-int openbuttonstate_full = 0;
 int closebuttonstate_full = 0;
+int openbuttonstate = 0;
 
 // Initialization of servo positions
 int index_pos = 0;
@@ -43,30 +40,21 @@ void setup() {
 }
 
 void loop() {
-    openbuttonstate_partial = digitalRead(OPENBUTTON_PARTIAL);
     closebuttonstate_partial = digitalRead(CLOSEBUTTON_PARTIAL);
-    openbuttonstate_full = digitalRead(OPENBUTTON_FULL);
     closebuttonstate_full = digitalRead(CLOSEBUTTON_FULL);
+    openbuttonstate = digitalRead(OPENBUTTON);
 
     fsrVal = analogRead(FSR_PIN);
     
-    if (openbuttonstate_partial == HIGH){
-        openHand_partial();
-    }
     if (closebuttonstate_partial == HIGH){
         closeHand_partial();
-    }
-    if (openbuttonstate_full == HIGH){
-        openHand_full();
     }
     if (closebuttonstate_full == HIGH){
         closeHand_partial();
     }
-}
-
-void openHand_partial() {
-    index_pos = 0;
-    indexServo.write(index_pos);
+    if (openbuttonstate == HIGH){
+        openHand();
+    }
 }
 
 void closeHand_partial() {
@@ -76,16 +64,16 @@ void closeHand_partial() {
     }
 }
 
-void openHand_full() {
-    rem_pos = 0;
-    index_pos = 0;
-    remServo.write(rem_pos);
-    indexServo.write(index_pos);
-}
-
 void closeHand_full() {
     index_pos = index_full_threshold;
     rem_pos = rem_full_threshold;
     indexServo.write(index_pos);
     remServo.write(rem_pos);
+}
+
+void openHand() {
+    rem_pos = 0;
+    index_pos = 0;
+    remServo.write(rem_pos);
+    indexServo.write(index_pos);
 }
